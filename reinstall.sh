3b3a4942c15578fdb9bd96524394da3287ad7c35
NerdOfCode <admin@nerdofcode.com>
2018-09-25 00:42:45 -0500

    Update reinstall.sh
    New reinstallation script is a bit more advanced... It even creates a simple clone of the old R-Shell config!

--- reinstall.sh
+++ reinstall.sh
@@ -1,14 +1,54 @@
 #!/bin/bash
 
-mv $0 ../
+#Safely go to parent directory, then perform a reinstall via Git
 
-rm -rf *
-rm -rf .*
+if [[ "$1" == "--help" ]]
+then
+        echo "This script reinstalls the Restricted-Shell..."
+        echo "This script creates a clone of the current Restricted-Shell in ~/.r-shell/clones/{RANDOM}"
+        exit 0
+fi
 
-git clone git@github.com:NerdOfCode/Restricted-Shell.git .
+root_check(){
 
-clear
+        if [[ "$1" == "--no-root" ]]
+        then
+                return 
+        fi
+
+        if [[ "$(whoami)" != "root" ]]
+        then
+                echo "This script will most likely need root."
+                echo "Pass '--no-root' if you're willing to risk it."
+                exit -1
+        fi
+
+
+}
+
+root_check $@
+
+RANDOM_NUMB=$RANDOM
+
+cd ../
+
+mkdir -p ~/.r-shell/clones/
+
+cp -r Restricted-Shell/ ~/.r-shell/clones/$RANDOM_NUMB
+
+sudo rm -rf Restricted-Shell/*
+
+git clone https://github.com/NerdOfCode/Restricted-Shell.git Restricted-Shell/ --quiet
+
+cd Restricted-Shell/
+
+#Reset Git completely 
+
+#git reset --hard REV
+
+#clear
 
 echo "Please run ./run.sh"
+echo "Cloned old R-Shell config in ~/.r-shell/clones/$RANDOM_NUMB"
 
 exit 0
