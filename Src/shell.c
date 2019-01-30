1a5716cfe5288cc988e360ce9b4191fecd4788dc
NerdOfCode <admin@nerdofcode.com>
2019-01-30 19:09:18 +0000

    Rewrote Reinstall.sh
    Improved readability and functionality of reinstall.sh by completely rewriting it.

--- Src/shell.c
+++ Src/shell.c
@@ -1,6 +1,6 @@
 /*
   This file is part of the Restricted-Shell distribution (https://github.com/NerdOfCode/Restricted-Shell).
-  Copyright (c) 2018 NerdOfCode.
+  Copyright (c) 2019 NerdOfCode.
  
   This program is free software: you can redistribute it and/or modify  
   it under the terms of the GNU General Public License as published by  
--- reinstall.sh
+++ reinstall.sh
@@ -1,54 +1,125 @@
-#!/bin/bash
+#!/bin/bash 
+: '                                                                                                                                                                                                                 This file is part of the Restricted-Shell distribution (https://github.com/NerdOfCode/Restricted-Shell).                                                                                                   Copyright (c) 2019 NerdOfCode.                                                                                                                                                                      																									 	     This program is free software: you can redistribute it and/or modify                                                                                                                              
+         it under the terms of the GNU General Public License as published by                                                                                                                              
+         the Free Software Foundation, version 3.                                                                                                                                                                   This program is distributed in the hope that it will be useful, but                                                                                                                                        WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                                                                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                                                                                           General Public License for more details.                                                                                                                                                                   You should have received a copy of the GNU General Public License                                                                                                                                          along with this program. If not, see <http://www.gnu.org/licenses/>.                                                                                                                              '
+# Author: NerdOfCode
+# Purpose: Perform a reinstallation of this Shell
+# Updated: 1/29/18
 
-#Safely go to parent directory, then perform a reinstall via Git
+# Location of where to automatically backup current configuration upon reinstall
+# Assume that the location starts with --> ~/
+readonly clone_location=".r-shell/clones/"
 
-if [[ "$1" == "--help" ]]
-then
-        echo "This script reinstalls the Restricted-Shell..."
-        echo "This script creates a clone of the current Restricted-Shell in ~/.r-shell/clones/{RANDOM}"
-        exit 0
-fi
+# ANSI color text #
+readonly red_text="\033[0;31m"
+readonly yellow_text="\033[0;33m"
+readonly green_text="\033[0;32m"
+readonly reset_text="\033[0m"
 
 root_check(){
-
-        if [[ "$1" == "--no-root" ]]
-        then
-                return 
-        fi
-
-        if [[ "$(whoami)" != "root" ]]
-        then
-                echo "Error: Not Root!"
-                echo "Pass '--no-root' if you're willing to risk it."
-                exit -1
-        fi
-
-
+    if [[ "$(whoami)" != "root" ]] && [[ "$no_root" != true ]]
+    then
+        echo -e "${red_text}Error:${reset_text} unprivileged user"
+        echo -e "\t${yellow_text}Note:${reset_text} Pass '--no-root' to bypass this"
+        exit -1
+    fi
 }
 
-root_check $@
+## Generate the psuedo-random parent directory name of the actual old cloned config ##
+create_config_backup(){
+    # Absolute file path to the config
+    local absolute_config_backup_location="${clone_location}$RANDOM/"
+    if [[ -d ~/$clone_location ]]
+    then
+	if [[ ! -d ${absolute_config_backup_location} ]]
+	then
+	    echo -e "${yellow_text}Note:${reset_text} Cloning contents..."
+	    mkdir -p ~/${absolute_config_backup_location}
+	    cp -r ../Restricted-Shell/ ~/${absolute_config_backup_location}
+	else
+	    # A directory name collision has occured... Most likely :)
+	    create_config_backup
+	fi
+    else
+	echo -e "${yellow_text}Note:${reset_text} Creating config backup directory @ [~/${clone_location}]"
+	mkdir -p ~/$clone_location
+	
+	if [[ -d ~/$clone_location ]]
+	then
+	    echo -e "${yellow_text}Note:${reset_text} Creating current config backup directory @ [~/${absolute_config_backup_location}]"
+	    mkdir -p ~/$absolute_config_backup_location
+	else
+	    echo -e "${red_text}Error:${reset_text} Creating directory @ [~/${clone_location}]"
+	    exit -1
+	fi 
+    fi
+    echo -e "${green_text}Success:${reset_text} Created backup @ [~/${absolute_config_backup_location}]"
+}
 
-RANDOM_NUMB=$RANDOM
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+	-h | --help)
+	    echo "This script reinstalls the Restricted-Shell..."
+	    echo ""
+	    echo "Current arguements include:"
+	    echo -e "\t\t-h/--help --> Display this message."
+	    echo -e "\t\t-nr/--no-root --> Run as non-privileged user."
+	    exit 0
+	    ;;
+	-nr | --no-root)
+	    echo -e "${yellow_text}Note:${reset_text} Running as non-privileged user"
+	    no_root=true
+	    ;;
+	-nb | --no-backup)
+	    echo -e "${red_text}Warning:${reset_text} Not creating backup"
+	    no_backup=true
+	    ;;
+    esac
+    shift
+done
 
-cd ../
+# Let's check the permissions of the running user
+root_check
 
-mkdir -p ~/.r-shell/clones/
+# Let's backup the current config, unless the user has specified else...
+if [[ "$no_backup" != true ]]
+then
+    create_config_backup
+else
+    # Proceed with reinstall - No backup
+    :
+fi
 
-cp -r Restricted-Shell/ ~/.r-shell/clones/$RANDOM_NUMB
+read -p "Ready to reinstall(y/n): " yesno
+while true
+do
+    if [[ "$yesno" == "y" ]] || [[ "$yesno" == "Y" ]]
+    then
+	# Ready to reinstall
+	break
+    elif [[ "$yesno" == "n" ]] || [[ "$yesno" == "N"  ]]
+    then
+	# Not quite
+	echo -e "${yellow_text}Note:${reset_text} Exiting now"
+	exit 0
+	break
+    else
+	# Unrecognized option
+	echo -e "${yellow_text}Note:${reset_text} Unrecognized option $yesno"
+	continue
+    fi
+done
 
-sudo rm -rf Restricted-Shell/*
+cd ../ && rm -rf Restricted-Shell/
+git clone https://github.com/NerdOfCode/Restricted-Shell.git --quiet
 
-git clone https://github.com/NerdOfCode/Restricted-Shell.git Restricted-Shell/ --quiet
+if [[ -d "Restricted-Shell/"  ]]
+then
+    echo -e "${green_text}Success:${reset_text} Reinstalled"
+else
+    echo -e "${red_text}Error:${reset_text} An unknown problem has occured while cloning"
+fi
 
-cd Restricted-Shell/
-
-#Reset Git completely 
-
-#git reset --hard REV
-
-#clear
-
-echo "Please run ./run.sh"
-echo "Cloned old R-Shell config in ~/.r-shell/clones/$RANDOM_NUMB"
-
-exit 0
+# Reset previously directory setting
+cd -
