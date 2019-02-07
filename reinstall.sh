c704a026825e8a6dca032e2b869efe45646cf8c1
NerdOfCode <admin@nerdofcode.com>
2019-02-07 12:14:54 -0600

    Fixed Formatting

--- reinstall.sh
+++ reinstall.sh
@@ -1,10 +1,20 @@
 #!/bin/bash 
-: '            
-         This file is part of the Restricted-Shell distribution (https://github.com/NerdOfCode/Restricted-Shell).                                                                                                   Copyright (c) 2019 NerdOfCode.																				
-																										    This program is free software: you can redistribute it and/or modify                                                                                                                          
-         it under the terms of the GNU General Public License as published by                                                                                                                              
-         the Free Software Foundation, version 3.                                                                                                                                                                   This program is distributed in the hope that it will be useful, but                                                                                                                                        WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                                                        
-         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU                                                                                                                                           General Public License for more details.                                                                                                                                                                   You should have received a copy of the GNU General Public License                                                                                                                                          along with this program. If not, see <http://www.gnu.org/licenses/>.                                                                                                                              '
+: '      
+         This file is part of the Restricted-Shell distribution (https://github.com/NerdOfCode/Restricted-Shell).
+         Copyright (c) 2019 NerdOfCode.
+  
+         This program is free software: you can redistribute it and/or modify  
+         it under the terms of the GNU General Public License as published by  
+         the Free Software Foundation, version 3.
+ 
+         This program is distributed in the hope that it will be useful, but 
+         WITHOUT ANY WARRANTY; without even the implied warranty of 
+         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+         General Public License for more details.
+ 
+         You should have received a copy of the GNU General Public License 
+         along with this program. If not, see <http://www.gnu.org/licenses/>.
+'
 # Author: NerdOfCode
 # Purpose: Perform a reinstallation of this Shell
 # Updated: 1/30/18
